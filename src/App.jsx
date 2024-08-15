43caecb02ed8a741bbedbed6603c90f6bf97c21d
AnishGzp <anishsinghgzp1024@gmail.com>
2024-08-15 13:35:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ImageSlider from "./components/ImageSlider";
+
+function App() {
+  return (
+    <>
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"5"}
+      />
+    </>
+  );
+}
+
+export default App;
